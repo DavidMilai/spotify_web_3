b08cbe3716ee225fc4fec19d3770bafcc81e1be6
DavidMilai <davidmilai67@gmail.com>
2022-04-21 09:35:56 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Album from './pages/Album';
-import './App.css';
+import Album from "./pages/Album";
+import "./App.css";
+import { Layout } from "antd";
+const { Footer, Sider, Content } = Layout;
 
 const App = () => {
-  return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/album" element={<Album />} />
-    </Routes>
-  )
+  return (
+    <Layout>
+      <Sider>left sidebar</Sider>
+      <Content>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/album" element={<Album />} />
+        </Routes>
+      </Content>
+    </Layout>
+  );
 };
 
 export default App;
